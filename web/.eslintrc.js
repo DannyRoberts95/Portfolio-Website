90b866d6b30042bacaceefbdb7c60f25aafec931
dannyroberts95 <danielrdhr@gmail.com>
2022-09-20 11:49:00 +0100

    removed lint packages

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -1,17 +1,32 @@
 module.exports = {
-  extends: [
-    'next',
-    'standard',
-    'standard-react',
-    'prettier',
-    'plugin:import/warnings',
-    'plugin:react/recommended',
-  ],
+  env: {
+    commonjs: true,
+    node: true,
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'next'],
+  globals: {},
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'import', 'react-hooks'],
+  ignorePatterns: ['node_modules/'],
   rules: {
-    'object-curly-spacing': ['error', 'never'],
-    'comma-dangle': 0,
-    '@next/next/no-img-element': 0,
-    'react/react-in-jsx-scope': 0,
     'react/prop-types': 0,
+    'react/react-in-jsx-scope': 0,
+    'no-undef': 0,
+    'no-unsafe-optional-chaining': 0,
+  },
+  settings: {
+    react: {
+      version: 'latest', // "detect" automatically picks the version you have installed.
+    },
   },
 }
