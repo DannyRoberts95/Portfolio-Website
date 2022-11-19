795b3deece5109423013258c50383066107817c0
dannyroberts95 <danielrdhr@gmail.com>
2022-11-19 16:21:48 +0000

    updated packages

--- web/client.js
+++ web/client.js
@@ -4,9 +4,9 @@ import sanityClient from '@sanity/client'
 const isProduction = process.env.NODE_ENV || 'development'
 
 const client = sanityClient({
-  projectId: process.env.SANITY_PROJECT_ID,
-  dataset: process.env.SANITY_DATASET,
-  apiVersion: process.env.SANITY_API_VERSION,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
   useCdn: isProduction,
 })
 
