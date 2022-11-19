e02977ddddd8ee84ffd33a844f276fa9f2ea00e8
dannyroberts95 <danielrdhr@gmail.com>
2022-11-19 16:52:30 +0000

    spell fix again...

--- web/client.js
+++ web/client.js
@@ -6,7 +6,7 @@ const isProduction = process.env.NODE_ENV || 'development'
 const client = sanityClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-  apiVersion: process.NEXT_PUBLIC_env.SANITY_API_VERSION,
+  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
   useCdn: isProduction,
 })
 
