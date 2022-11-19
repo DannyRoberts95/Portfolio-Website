3d2ef57708367811427e5b7ba65887de8e238dc2
dannyroberts95 <danielrdhr@gmail.com>
2022-11-19 12:55:54 +0000

    client uses env vars

--- web/client.js
+++ web/client.js
@@ -1,12 +1,13 @@
 import sanityClient from '@sanity/client'
 
-const isProduction = process.env.VERCEL_ENV === 'production'
+// const isProduction = process.env.VERCEL_ENV === 'production'
+const isProduction = process.env.NODE_ENV || 'development'
 
 const client = sanityClient({
-  projectId: '9rg3r6lx',
-  dataset: 'production',
+  projectId: process.env.SANITY_PROJECT_ID,
+  dataset: process.env.SANITY_DATASET,
+  apiVersion: process.env.SANITY_API_VERSION,
   useCdn: isProduction,
-  apiVersion: '2022-01-31',
 })
 
 export default client
